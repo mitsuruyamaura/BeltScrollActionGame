3b0942baa8e01d8811153c9a3e2cfd2cd9ed8ff2
mitsuru_yamaura <komayou326@gmail.com>
2020-10-07 18:20:45 +0900

    敵のプレファブ追加。敵用スクリプトを作成。

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    public bool isDebug;
+
+    public void SetUpEnemy(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+
+
+    void Start()
+    {
+        if (isDebug) {
+            StartCoroutine(DestroyEnemy(3.0f));
+        }
+    }
+
+    private IEnumerator DestroyEnemy(float waitTime = 0.0f) {
+        yield return new WaitForSeconds(waitTime);
+        gameManager.RemoveEmenyList(gameObject);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
