f84e6cec3b72195b6dba044401976738ccdd2395
mitsuru_yamaura <komayou326@gmail.com>
2020-10-07 19:14:30 +0900

    GameManager作成。Playerの移動制限にGameManagerでの設定値を反映。 エリア管理用のスクリプタブル・オブジェクト作成。

---
+++ Assets/Scripts/StageList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "StageList", menuName = "Create StageList")]
+public class StageList : ScriptableObject { 
+
+    public List<StageData> stageDatas = new List<StageData>();
+
+    [Serializable]
+    public class StageData {
+        public int stageNo;
+        public List<AreaData> areaDatas = new List<AreaData>();
+    }
+
+    [Serializable]
+    public class AreaData {
+        public int areaNo;
+        public float startPos;
+        public float endPos;
+        public int[] appearNum;     // 敵の番号を入れておく
+        
+    }
+}
